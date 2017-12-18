cc0fc99376ccc39d8a635fdc70bbcd9faacbae0a
luiz <oliveiraluiz@gmail.com>
2017-12-18 16:26:51 -0200

    mudança lab banco de dados2

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 echo "<h1>Openshift Workshop v2.0</h1> ";
-echo $_SERVER['SERVER_ADDR']
+echo $_SERVER['SERVER_ADDR'];
 
 echo "<br><hr>";
 echo "<h2>Cidades cadastradas no Banco de Dados:</h2>";
