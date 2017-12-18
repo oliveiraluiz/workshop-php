6cb8999dbc824a39766a8cf43cd52557e2ffeb9b
luiz <oliveiraluiz@gmail.com>
2017-12-18 17:09:53 -0200

    pipeline

---
+++ pipeline/jenkins-pipeline.groovy
@@ -0,0 +1,18 @@
+
+node('maven') {
+    stage('build') {
+        echo 'building app :)'
+        openshiftBuild(buildConfig: 'workshop-php', showBuildLogs: 'true')
+    }
+    stage('verify') {
+        echo 'dummy verification....'
+    }
+    stage('deploy') {
+        input 'Manual Approval'
+        openshiftDeploy(deploymentConfig: 'workshop-php')
+    }
+    stage('promoting to QA') {
+       echo 'fake stage...'
+       sleep 5 
+    }
+}
